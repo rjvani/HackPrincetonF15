55a3607a016e03010fb44bb7157105dd9a387c67
Rohan Jadvani <rj1030@gmail.com>
2015-11-14 18:27:28 -0500

    Update dashboard page

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
+@app.route('/index.html')
+def dashboard():
+    return homepage()
+
+@app.route('/maps.html')
+def maps():
+    return render_template("maps.html")
+
+@app.route('/works.html')
+def works():
+    return render_template("works.html")
+
 if __name__ == "__main__":
     app.run()
